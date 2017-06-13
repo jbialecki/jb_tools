6332451b5fe7ad82e9086e915c0e95a43df72908
Jaroslaw Bialecki <jaroslaw.bialecki@wdc.com>
2017-06-13 08:21:28 +0200

    log_convert: in/out file names as call parameters

--- log_convert/log_convert.cpp
+++ log_convert/log_convert.cpp
@@ -189,10 +189,30 @@ void LogLine::printNewThr(ostringstream &o) const
 }
 int main(int argc, char* argv[])
 {
+	if(argc != 3)
+	{
+		cout << "Syntax error. Correct syntax:" << endl;
+		cout << "log_convert in_file out_file" << endl;
+		return 1;
+	}
+
+	ifstream f_in(argv[1]);
+	if(!f_in.is_open())
+	{
+		cout << "in_file not found: " << argv[1] << endl;
+		return 1;
+	}
+
+
+	ofstream f_out(argv[2]);
+	if(!f_out.is_open())
+	{
+		cout << "Can not create out_file: " << argv[2] << endl;
+		return 1;
+	}
+
 	string line;
 	list<LogLine> lines;
-	ifstream f_in("c:\\prj\\logs\\Read_WDT\\Read_WDT.txt");
-	ofstream f_out("c:\\prj\\logs\\Read_WDT\\processed.txt");
 	int i = 0;
 	while(getline(f_in, line))
 	{
