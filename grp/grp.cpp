59667ba8ef941bed560842c261956083247f9a5d
Jaroslaw Bialecki <jaroslaw.bialecki@wdc.com>
2017-06-14 13:57:56 +0200

    grp: printing multiple lines

--- grp/grp.cpp
+++ grp/grp.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
+void printSyntaxMsg()
+{
+	cout << "Syntax error. Correct syntax:" << endl;
+	cout << "grp pattern in_file [+printLines]" << endl;
+}
+
 int main(int argc, char* argv[])
 {
-	if(argc != 3)
+	if(argc < 3 || argc > 4)
 	{
-		cout << "Syntax error. Correct syntax:" << endl;
-		cout << "grp pattern in_file" << endl;
+		printSyntaxMsg();
 		return 1;
 	}
 	ifstream f_in(argv[2]);
@@ -18,12 +24,36 @@ int main(int argc, char* argv[])
 		cout << "in_file not found: " << argv[2] << endl;
 		return 1;
 	}
+	int printLines = 1;
+	if(argc == 4)
+	{
+		printLines = 0;
+		if(argv[3][0] != '+')
+			printSyntaxMsg();
+		string str(&argv[3][1]);
+		istringstream ii(str);
+		ii >> printLines;
+		if(printLines == 0)
+		{
+			cout << "printLines must be positive number" << endl;
+			return 1;
+		}
+	}
 	
 	string line;
 	while(getline(f_in, line))
 	{
 		if(line.find(argv[1]) != -1)
+		{
 			cout << line << endl;
+			for(int i=1; i<printLines; i++)
+			{
+				getline(f_in, line);
+				cout << line << endl;
+			}
+			if(printLines > 1)
+				cout << endl;
+		}
 	}
 	return 0;
 }	
